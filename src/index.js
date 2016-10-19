c0f151a91f78ebaefcf4ec35d54861dbe4f7bc49
C.T. Lin <chentsulin@gmail.com>
2016-10-19 18:12:55 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const { Component } = React;
+
+class TodoApp extends Component {
+  render() {
+    return <div>Hello</div>
+  }
+}
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
