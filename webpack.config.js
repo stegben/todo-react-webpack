1c94c1fd80ad3d605399564901a5a64215fc70cd
cph <stegben.benjamin@gmail.com>
2016-10-31 19:59:38 +0800

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+    }, {
+      test: /\.css$/,
+      loaders: ['style', 'css-loader'],
+    }],
+  },
+};
